8148c20647ad64f18cc03a29f14c7f1cf4c41650
Kadri Jarvpold <kadrijarvpold@Kadris-Air.home>
2020-09-20 19:59:01 +0100

    add css shopitem, add css button to etsy

--- src/Components/Shopitem.js
+++ src/Components/Shopitem.js
@@ -26,11 +26,11 @@ class Shopitem extends Component {
             <>
                 <div className="shopitem">
                     {this.state.images && this.state.images.results.map(image => <img src={image.url_fullxfull} />)}
-                    <p>{this.props.productInfo.title}</p>
+                    <h3>{this.props.productInfo.title}</h3>
                     <p>{this.props.productInfo.description}</p>
                     <p>{this.props.productInfo.quantity}</p>
                     <p>{this.props.productInfo.price + this.props.productInfo.currency_code}</p>
-                    <a href={this.props.productInfo.url}><div>Go to Etsy</div></a>
+                    <a href={this.props.productInfo.url}><button className="etsybutton">Buy on Etsy</button></a>
                 </div>
             </>
         )
@@ -39,3 +39,6 @@ class Shopitem extends Component {
 
 export default Shopitem;
 
+//<a href="https://react.school" target="_blank">
+//<Button> Link Button </Button>
+//</a>
