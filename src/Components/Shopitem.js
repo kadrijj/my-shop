230526bffad56c5ae426b174ce851c46cc81d0cd
Kadri Jarvpold <kadrijarvpold@Kadris-Air.home>
2020-09-20 19:07:05 +0100

    css footer

--- src/Components/Shopitem.js
+++ src/Components/Shopitem.js
@@ -24,15 +24,13 @@ class Shopitem extends Component {
         console.log(this.props)
         return (
             <>
-                <div className="shop">
-                    <div className="shopitem">
-                        {this.state.images && this.state.images.results.map(image => <img src={image.url_fullxfull} />)}
-                        <p>{this.props.productInfo.title}</p>
-                        <p>{this.props.productInfo.description}</p>
-                        <p>{this.props.productInfo.quantity}</p>
-                        <p>{this.props.productInfo.price + this.props.productInfo.currency_code}</p>
-                        <a href={this.props.productInfo.url}><div>Go to Etsy</div></a>
-                    </div>
+                <div className="shopitem">
+                    {this.state.images && this.state.images.results.map(image => <img src={image.url_fullxfull} />)}
+                    <p>{this.props.productInfo.title}</p>
+                    <p>{this.props.productInfo.description}</p>
+                    <p>{this.props.productInfo.quantity}</p>
+                    <p>{this.props.productInfo.price + this.props.productInfo.currency_code}</p>
+                    <a href={this.props.productInfo.url}><div>Go to Etsy</div></a>
                 </div>
             </>
         )
@@ -41,13 +39,3 @@ class Shopitem extends Component {
 
 export default Shopitem;
 
-//results
-//image?
-
-//what is needed from API:
-//title: "Presunto Fumado"
-//description: "Wear your statement.
-//quantity: 1
-//currency_code: "EUR"
-//price: "20.00"
-//url: 
