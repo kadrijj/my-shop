af2275f1dfbb1662de2f3dbb658d01d03143c688
Kadri Jarvpold <kadrijarvpold@Kadris-Air.home>
2020-09-26 10:47:20 +0100

    css contact form

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,36 +1,60 @@
 import React from 'react';
-import emailjs from 'emailjs-com';
-import { init } from 'emailjs-com';
-init("user_WvAzMkmobmY8Uth3Uf1js");
+import { SocialIcon } from 'react-social-icons';
 
-function Contact() {
-
-    function sendEmail(e) {
-        e.preventDefault();
-
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', e.target,
-            'user_WvAzMkmobmY8Uth3Uf1js')
-            .then((result) => {
-                console.log(result.text);
-            }, (error) => {
-                console.log(error.text);
-            });
+class Contact extends React.Component {
+    constructor(props) {
+        super(props);
+        this.submitForm = this.submitForm.bind(this);
+        this.state = {
+            status: ""
+        };
     }
 
-    return (
-        <form className="contact-form" onSubmit={sendEmail}>
-            <div className="form-input"> <input type="hidden" name="contact_number" />
-                <label>Name</label>
-                <input type="text" name="user_name" />
-                <label>Email</label>
-                <input type="email" name="user_email" />
-            </div>
-            <div className="form-input"><label>Message</label>
-                <textarea name="message" />
-            </div>
-            <input className="submit-button" type="submit" value="Send" />
-        </form>
-    );
+    render() {
+        const { status } = this.state;
+        return (
+            <>
+                <div className="centered-contact">
+                    <div className="contact-container">
+                        <h3>Contact Us</h3>
+                        <p>We'd love to hear from you! Please fill in the form below and we will get back to you shortly. We are also available on Instagram - let's connect in the DMs!</p>
+                        <form
+                            onSubmit={this.submitForm}
+                            action="https://formspree.io/mjvpvnld"
+                            method="POST"
+                        >
+                            <label>Email:</label>
+                            <input type="email" name="email" />
+                            <label>Message:</label>
+                            <textarea id="subject" name="subject" placeholder="Write something.."></textarea>
+                            {status === "SUCCESS" ? <p>Thank you for sending us a message! We will get back to you very soon!</p> : <button className="submit-button">Send</button>}
+                            {status === "ERROR" && <p>Ooops! There was an error. Did you insert an email address?</p>}
+                        </form>
+                        <li><SocialIcon url="https://www.instagram.com/jkadrij/" /></li>
+                    </div>
+                </div>
+            </>
+        );
+    }
+
+    submitForm(ev) {
+        ev.preventDefault();
+        const form = ev.target;
+        const data = new FormData(form);
+        const xhr = new XMLHttpRequest();
+        xhr.open(form.method, form.action);
+        xhr.setRequestHeader("Accept", "application/json");
+        xhr.onreadystatechange = () => {
+            if (xhr.readyState !== XMLHttpRequest.DONE) return;
+            if (xhr.status === 200) {
+                form.reset();
+                this.setState({ status: "SUCCESS" });
+            } else {
+                this.setState({ status: "ERROR" });
+            }
+        };
+        xhr.send(data);
+    }
 }
 
 export default Contact;
